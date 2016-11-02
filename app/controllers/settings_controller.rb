30d10e5f3d35250ee6a5cfa3f84f0e0b19d79701
matthewrpacker <matthewrpacker@gmail.com>
2016-11-02 14:16:20 -0600

    Complete styling for mobile

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,8 @@
 class SettingsController < ApplicationController
   def index
     @players = Player.all
+    @orders = Order.all
+
+    @orders = Order.last(3)
   end
 end
--- app/helpers/utils.rb
+++ app/helpers/utils.rb
@@ -1,5 +1,5 @@
 module Utils
   def formatted_phone
-    phone.tr('+1', '').insert(3, '-').insert(7, '-')
+    phone.insert(3, '-').insert(7, '-')
   end
 end
