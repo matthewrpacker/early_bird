b4daefa92dbdb83879c3f9b7d0268909cf5c8c9c
Matt Packer <matthewrpacker@gmail.com>
2016-11-02 15:53:10 -0600

    Modify google redirect (#40)
    * Modify log in with google redirect
    - Implement request.referer to redirect user based on most recent location
    - Require mobile user to log in before viewing tee times
    - Require web user to log in before booking a tee time

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to settings_path
+    redirect_to request.referer
   end
 
   def destroy
