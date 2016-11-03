3e48e85ffbbe76a0113637404c10f817b9ef6744
matthewrpacker <matthewrpacker@gmail.com>
2016-11-03 10:53:05 -0600

    Refactor OrdersController - Move Twilio logic to OrdersHelper - Move booking details to OrdersHelper

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,14 @@
 class OrdersController < ApplicationController
+  include OrdersHelper
+
   def index
-    @orders = Order.all.order(:tee_off_at)
+    @orders = current_user.orders.all.order(:tee_off_at)
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @tee_time = TeeTime.find_by_course_tee_time(params[:course_id], params[:id])
-    TeeTime.update_quantity(params[:course_id], params[:id], params[:quantity])
-    @remaining_tee_time = TeeTime.find_by_course_tee_time(params[:course_id], params[:id])
+    find_booking_details
 
-    @user_tee_time_quantity = @tee_time.quantity-@remaining_tee_time.quantity
-
-    @order = Order.create(
+    @order = current_user.orders.create(
       user_id: current_user.id,
       course_name: @course.name,
       course_id: @course.id,
@@ -20,15 +17,7 @@ class OrdersController < ApplicationController
       unit_price: @tee_time.unit_price
     )
 
-    account_sid = ENV['TWILIO_ACCOUNT_SID']
-    auth_token = ENV['TWILIO_AUTH_TOKEN']
-
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    message = @client.account.messages.create(:body => "Thanks for booking with EarlyBird! Your code is #{@tee_time.id}. Present this code to the starter and you are ready to play.  Enjoy your round!",
-        :to => ENV['MY_NUMBER'],
-        :from => ENV['MY_TWILIO_NUMBER'])
-
-    @message = message
+    send_text_confirmation
     redirect_to orders_path
   end
 end
